42fd060ec4e9883b5d6b6dd63725c0e3b5c9616f
Anthony Dunford <trashzy21@gmail.com>
2022-05-27 00:06:32 +0100

    add in afterShifts to filter out None values

--- Board.fs
+++ Board.fs
@@ -12,20 +12,28 @@ module Board =
     module getSquares =
         let private isOnBoard (position: int * int) (board: board<'Piece>) : bool =
             let x, y = position
-            x >= 0 && x < Array2D.base1 board && y >= 0 && y < Array2D.base2 board
+            x >= 0 && x < Array2D.length1 board && y >= 0 && y < Array2D.length2 board
         let afterShift (shift: int * int) (start: int * int) (board: board<'Piece>) : square<'Piece> option =
             let (i, j), (x, y) = shift, start
             if isOnBoard (x+i, y+j) board then
                 Some board.[x+i,y+j]
             else None
+        let afterShifts (start: int * int) (board: board<'Piece>) (shifts: (int*int) list) : square<'Piece> list =
+            shifts
+            |> List.map (fun shift -> afterShift shift start board)
+            |> List.filter Option.isSome
+            |> List.map Option.get
         let rec afterRepeatedShift (shift: int * int) (start: int * int) (stopAt: ('Piece -> bool) option) (board: board<'Piece>) : square<'Piece> list =
             match afterShift shift start board with
             | None -> []
             | Some square ->
                 match stopAt, square.piece with
-                | Some stopAtPieceFunc, Some piece when stopAtPieceFunc piece -> []
+                | Some stopAtPieceFunc, Some piece when stopAtPieceFunc piece -> [square]
                 | _ -> List.append [square] <| afterRepeatedShift shift square.coordinates stopAt board
         
+        let adjacent (start: square<'Piece>) (board: board<'Piece>) : square<'Piece> list =
+            [(0,1); (1,1); (1,0); (1,-1); (0,-1); (-1,0); (-1,-1); (-1,1)]
+            |> afterShifts start.coordinates board
         let getDiagonals (start: square<'Piece>) (stopAt: ('Piece -> bool) option) (board: board<'Piece>) : square<'Piece> list =
             [(-1,1); (1,1); (-1,-1); (1,-1)]
             |> List.fold (fun s direction ->
@@ -39,33 +47,30 @@ module Board =
         let afterAllShiftDirections (start: square<'Piece>) (i: int) (j: int) (board: board<'Piece>) : square<'Piece> list =
             if i = 0 || j = 0 then
                 [
-                    afterShift start.coordinates (j+i,0) board;
-                    afterShift start.coordinates (-j-i,0) board;
-                    afterShift start.coordinates (0,i+j) board;
-                    afterShift start.coordinates (0,-i-j) board
+                    (j+i,0);
+                    (-j-i,0);
+                    (0,i+j);
+                    (0,-i-j)
                 ]                
             elif i = j then
                 [
-                    afterShift start.coordinates (i,i) board;
-                    afterShift start.coordinates (i,-i) board;
-                    afterShift start.coordinates (-i,i) board;
-                    afterShift start.coordinates (-i,-i) board
+                    (i,i);
+                    (i,-i);
+                    (-i,i);
+                    (-i,-i)
                 ]
             else
                 let x, y = 
                     if i > j then i, j
                     else j, i
                 [
-                    afterShift start.coordinates (x,y) board;
-                    afterShift start.coordinates (x,-y) board;
-                    afterShift start.coordinates (-x,y) board;
-                    afterShift start.coordinates (-x,-y) board
-                    afterShift start.coordinates (y,x) board;
-                    afterShift start.coordinates (y,-x) board;
-                    afterShift start.coordinates (-y,x) board;
-                    afterShift start.coordinates (-y,-x) board
+                    (x,y);
+                    (x,-y);
+                    (-x,y);
+                    (-x,-y)
+                    (y,x);
+                    (y,-x);
+                    (-y,x);
+                    (-y,-x)
                 ]
-            |> List.filter Option.isSome
-            |> List.map Option.get
-                
-                
+            |> afterShifts start.coordinates board
