91214082d3490dc44beb7710a35c08cc0ec3b220
Anthony Dunford <trashzy21@gmail.com>
2022-06-08 19:49:44 +0100

    Add getFromName and fix getName

--- Square.fs
+++ Square.fs
@@ -5,7 +5,12 @@ type square<'Piece> = {piece: 'Piece option; coordinates: int * int}
 module Square =
     let getName (square: square<'piece>) : string =
         let x, y = square.coordinates
-        sprintf "%c%d" ['a'..'h'].[x] y
+        sprintf "%c%d" ['a'..'h'].[x] (y+1)
+    let getFromName (name: string) : square<'Piece> =
+        let c, i = name[0], System.Char.GetNumericValue name[1] |> int
+        let j = List.findIndex (fun letter -> letter = c) ['a'..'h']
+        {piece = None; coordinates = (i, j)}
+
     let getPiece (square: square<'piece>) : 'piece =
         match square.piece with
         | None -> failwith $"No piece in on position {getName square}"
