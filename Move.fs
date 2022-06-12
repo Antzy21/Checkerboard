a51ca629b04c53490c513b45e1af30d6d11913f1
Anthony Dunford <trashzy21@gmail.com>
2022-06-12 12:00:15 +0100

    Add Move module and type

---
+++ Move.fs
@@ -0,0 +1,15 @@
+﻿namespace Checkerboard
+
+type move<'Piece> = square<'Piece> * square<'Piece>
+
+module Move =
+    let getShift (move: move<'Piece>) : int * int =
+        let start = (fst move).coordinates
+        let finish = (snd move).coordinates
+        (fst start - fst finish, snd start - snd finish)
+    let getTakenPiece (move: move<'Piece>) : 'Piece option =
+        snd move
+        |> fun square -> square.piece
+    let getMovedPiece (move: move<'Piece>) : 'Piece =
+        fst move
+        |> Square.getPiece
