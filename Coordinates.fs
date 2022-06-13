220792944e57d4cae4a9b56a1a5e2faacd60851f
Anthony Dunford <trashzy21@gmail.com>
2022-06-13 19:18:52 +0100

    Pull out coordinates from square

---
+++ Coordinates.fs
@@ -0,0 +1,11 @@
+﻿namespace Checkerboard
+
+type coordinates = int * int
+
+module Coordinates =
+    let getName ((i,j) : coordinates) : string =
+        sprintf "%c%d" ['a'..'z'].[i] (j+1)
+    let fromName (name: string) : coordinates =
+        let j, c = System.Char.GetNumericValue name[1] |> int, name[0]
+        let i = List.findIndex (fun letter -> letter = c) ['a'..'z']
+        (i, j)
--- Square.fs
+++ Square.fs
@@ -3,17 +3,9 @@
 type square<'Piece> = {piece: 'Piece option; coordinates: int * int}
 
 module Square =
-    let getName (square: square<'piece>) : string =
-        let x, y = square.coordinates
-        sprintf "%c%d" ['a'..'h'].[x] (y+1)
-    let getFromName (name: string) : square<'Piece> =
-        let j, c = System.Char.GetNumericValue name[1] |> int, name[0]
-        let i = List.findIndex (fun letter -> letter = c) ['a'..'h']
-        {piece = None; coordinates = (i, j-1)}
-
     let getPiece (square: square<'piece>) : 'piece =
         match square.piece with
-        | None -> failwith $"No piece in on position {getName square}"
+        | None -> failwith $"No piece in on position {Coordinates.getName square.coordinates}"
         | Some piece -> piece
     let updateWithPiece (piece: 'Piece) (square: square<'Piece>) : square<'Piece> =
         {piece = Some piece; coordinates = square.coordinates}
