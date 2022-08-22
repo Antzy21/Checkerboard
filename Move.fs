f02f21040ccc2db1bff7e0469cc1f1e72b320de9
Anthony Dunford <30522408+Antzy21@users.noreply.github.com>
2022-08-22 22:09:59 +0100

    Rename function

--- Move.fs
+++ Move.fs
@@ -7,7 +7,7 @@ module Move =
         let start = (fst move).coordinates
         let finish = (snd move).coordinates
         (fst start - fst finish, snd start - snd finish)
-    let getTakenPiece (move: move<'Piece>) : 'Piece option =
+    let getPieceAtDestination (move: move<'Piece>) : 'Piece option =
         snd move
         |> fun square -> square.piece
     let getMovedPiece (move: move<'Piece>) : 'Piece =
