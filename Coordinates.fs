a2d47f627079004aa44b4f3c1b4fe300dbe4df02
Anthony Dunford <30522408+Antzy21@users.noreply.github.com>
2022-08-23 21:59:10 +0100

    Add tryParse for coordinates

--- Coordinates.fs
+++ Coordinates.fs
@@ -9,5 +9,11 @@ module Coordinates =
         let j, c = System.Char.GetNumericValue name[1] |> int, name[0]
         let i = List.findIndex (fun letter -> letter = c) ['a'..'z']
         (i, j-1)
+    let tryParse (name: string) : coordinates option =
+        try
+            fromName name 
+            |> Some
+        with
+        _ -> None
     let afterShift ((i, j): int * int) ((x,y): coordinates) : coordinates =
         (x+i, y+j)
