269276b760d4cf3dfad55e27d3c711c9f96a035c
Anthony Dunford <30522408+Antzy21@users.noreply.github.com>
2022-09-03 17:37:06 +0100

    Add tests for Move module

---
+++ Checkerboard.Tests/Move.fs
@@ -0,0 +1,23 @@
+﻿module Tests.Move
+
+open Checkerboard
+open Xunit
+
+let exampleMove1 =
+    {piece = Some "myPiece1"; coordinates = (6,2)},
+    {piece = Some "myPiece2"; coordinates = (3,5)}
+
+[<Fact>]
+let ``getShift`` () =
+    let result = Move.getShift exampleMove1
+    Assert.Equal((-3, 3), result)
+    
+[<Fact>]
+let ``getMovedPiece`` () =
+    let result = Move.getMovedPiece exampleMove1
+    Assert.Equal("myPiece1", result)
+    
+[<Fact>]
+let ``getDestinationPiece`` () =
+    let result = Move.getPieceAtDestination exampleMove1
+    Assert.Equal(Some "myPiece2", result)
