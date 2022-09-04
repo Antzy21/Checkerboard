9f34b011d32193e2e7dc3cf5555a75d48595b85c
Anthony Dunford <30522408+Antzy21@users.noreply.github.com>
2022-09-04 16:12:22 +0100

    Add tests for undo move

--- Checkerboard.Tests/Board.fs
+++ Checkerboard.Tests/Board.fs
@@ -17,3 +17,31 @@ let ``Apply move to board`` () =
     Board.Update.applyMove Moves.example2 board
     Assert.Equal(board.[3,2], {piece = None; coordinates = Coordinates._3_2})
     Assert.Equal(board.[5,0], {piece = Some Pieces.example1; coordinates = Coordinates._5_0})
+    
+[<Fact>]
+let ``Apply capture move to board`` () =
+    let board : board<string> = Board.Create.empty 6
+    Board.Update.Square.withPiece Coordinates._3_2 Pieces.example1 board
+    Board.Update.Square.withPiece Coordinates._5_0 Pieces.example2 board
+    Board.Update.applyMove Moves.example1 board
+    Assert.Equal(board.[3,2], {piece = None; coordinates = Coordinates._3_2})
+    Assert.Equal(board.[5,0], {piece = Some Pieces.example1; coordinates = Coordinates._5_0})
+    
+[<Fact>]
+let ``Undo move on board`` () =
+    let board : board<string> = Board.Create.empty 6
+    Board.Update.Square.withPiece Coordinates._3_2 Pieces.example1 board
+    Board.Update.applyMove Moves.example1 board
+    Board.Update.undoMove Moves.example1 board
+    Assert.Equal(board.[3,2], {piece = Some Pieces.example1; coordinates = Coordinates._3_2})
+    Assert.Equal(board.[5,0], {piece = None; coordinates = Coordinates._5_0})
+    
+[<Fact>]
+let ``Undo capture move on board`` () =
+    let board : board<string> = Board.Create.empty 6
+    Board.Update.Square.withPiece Coordinates._3_2 Pieces.example1 board
+    Board.Update.applyMove Moves.example2 board
+    Board.Update.undoMove Moves.example2 board
+    Assert.Equal(board.[3,2], {piece = Some Pieces.example1; coordinates = Coordinates._3_2})
+    Assert.Equal(board.[5,0], {piece = Some Pieces.example2; coordinates = Coordinates._5_0})
+
