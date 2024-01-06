179e819bb9ab65a6b0d9c4870df9741582c579de
Anthony Dunford <Anthony@Dunford.uk.com>
2024-01-06 10:05:29 +0000

    Add initial CoordinatesCollection type and module

---
+++ Checkerboard/CoordinatesCollection.fs
@@ -0,0 +1,23 @@
+﻿namespace Checkerboard
+
+open System
+
+type coordinatesCollection = UInt64
+    
+module CoordinatesCollection =
+    
+    let construct () = 0UL
+
+    let toList (cc: coordinatesCollection) : coordinates List =
+        cc
+        |> BitMap.IsolateValues
+        |> List.map Coordinates.constructFromValue
+
+    let append (c: coordinates) (cc: coordinatesCollection) : coordinatesCollection =
+        cc ||| c.value
+
+    let remove (c: coordinates) (cc: coordinatesCollection) : coordinatesCollection =
+        cc &&& ~~~c.value
+
+    let filter (positiveMatches: coordinatesCollection) (cc: coordinatesCollection) : coordinatesCollection =
+        positiveMatches &&& cc
