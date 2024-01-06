7eecd101a8acca825abb7db20739b813196f748c
Anthony Dunford <trashzy21@gmail.com>
2024-01-06 14:21:34 +0000

    add appendResult

--- Checkerboard/CoordinatesCollection.fs
+++ Checkerboard/CoordinatesCollection.fs
@@ -1,11 +1,13 @@
 ﻿namespace Checkerboard
 
 open System
+open FSharp.Extensions
 
 type coordinatesCollection = UInt64
     
 module CoordinatesCollection =
     
+    /// Construct an empty coordinatesCollection
     let construct () = 0UL
 
     let toList (cc: coordinatesCollection) : coordinates List =
@@ -15,6 +17,12 @@ module CoordinatesCollection =
 
     let append (c: coordinates) (cc: coordinatesCollection) : coordinatesCollection =
         cc ||| c.value
+        
+    /// Appends a coordinates value if Ok, ignores if Error.
+    let appendResult (coordsResult: coordinates result) (cc: coordinatesCollection) : coordinatesCollection =
+        match coordsResult with
+        | Ok coords -> append coords cc
+        | Error _ -> cc
 
     let remove (c: coordinates) (cc: coordinatesCollection) : coordinatesCollection =
         cc &&& ~~~c.value
